ea177e58125e0f208c37b9eca3170366400877de
huijingj <vanessajin0426@gmail.com>
2025-06-03 15:51:31 -0700

    2 - Clerk user auth and DB sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>Home page content</h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
 import DesktopNavbar from "@/components/DesktopNavbar";
 import MobileNavbar from "@/components/MobileNavbar";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await currentUser();
+
+  if (user) await syncUser(); // POST
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
