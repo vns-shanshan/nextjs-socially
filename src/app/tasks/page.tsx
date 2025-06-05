d01a5307d8e911116b03536075e3524805d6be01
huijingj <vanessajin0426@gmail.com>
2025-06-05 09:57:55 -0700

    3 - Notifications Page

---
+++ src/app/tasks/page.tsx
@@ -0,0 +1,13 @@
+// const TasksPage = async () => {
+//   const response = await fetch("http://localhost:3000/api/tasks", {
+//     cache: "no-store",
+//   });
+
+//   const tasks = await response.json();
+
+//   console.log(tasks);
+
+//   return <div>TasksPage</div>;
+// };
+
+// export default TasksPage;
