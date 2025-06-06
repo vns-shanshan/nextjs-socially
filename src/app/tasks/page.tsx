702d43e984545bde25033ba07a77be45937c54bc
huijingj <vanessajin0426@gmail.com>
2025-06-05 17:30:58 -0700

    6 - minor changes

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,13 +1,13 @@
-// const TasksPage = async () => {
-//   const response = await fetch("http://localhost:3000/api/tasks", {
-//     cache: "no-store",
-//   });
+const TasksPage = async () => {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    cache: "no-store",
+  });
 
-//   const tasks = await response.json();
+  const tasks = await response.json();
 
-//   console.log(tasks);
+  console.log(tasks);
 
-//   return <div>TasksPage</div>;
-// };
+  return <div>TasksPage</div>;
+};
 
-// export default TasksPage;
+export default TasksPage;
